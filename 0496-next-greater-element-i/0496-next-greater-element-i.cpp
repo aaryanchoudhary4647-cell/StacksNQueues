d72f9f0accf5bb3a58e7289e4451994a5d84f586
Aaryan Choudhary <aaryanchoudhary4647@gmail.com>
2026-08-15 12:35:37 +0530

    Time: 0 ms (100%), Space: 13 MB (15.61%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
+    // as we have already done with the brute force approach , now we will use monotonic stack , where monotonic means that we are storing data in stack in a particular order.
+
+    // the intuition is that we will store the elements of nums2 from last and simultaneously we will store their next max in a map
+
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> mpp;
         vector<int> ans;
-        int n = nums1.size();
-        int m = nums2.size();
-        for(int i=0;i<n;i++){
-            int el = nums1[i];
-            for(int j=0;j<m;j++){
-                bool exists = false;
-                if(el==nums2[j]){
-                    for(int k=j;k<m;k++){
-                        if(nums2[k]>el){
-                            ans.push_back(nums2[k]);
-                            exists = true;
-                            break;
-                        }
+        stack<int> s;
+        for(int i=nums2.size()-1;i>=0;i--){
+            if(s.empty()){
+                mpp[nums2[i]] = -1;
+                s.push(nums2[i]);
+            }
+            else{
+                while(!s.empty()){
+                    if(s.top()>nums2[i]){
+                        mpp[nums2[i]] = s.top();
+                        s.push(nums2[i]);
+                        break;
                     }
-                    if(!exists){ans.push_back(-1);}
-                    
-                    break;
+                    else{s.pop();}
+                }
+                if(s.empty()){
+                    mpp[nums2[i]] = -1;
+                    s.push(nums2[i]);
                 }
             }
         }
 
+        for(int i=0;i<nums1.size();i++){
+            ans.push_back(mpp[nums1[i]]); 
+        }
+
         return ans;
+
     }
 };
