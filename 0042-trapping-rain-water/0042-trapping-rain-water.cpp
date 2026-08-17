80342a79edca4d32dd3c63048d48e4c2675d31d4
Aaryan Choudhary <aaryanchoudhary4647@gmail.com>
2026-08-17 11:21:06 +0530

    Time: 0 ms (100%), Space: 25.9 MB (93.75%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
-    // This is the brute force approach TC -> O(3N) and SC -> O(2N) 
-    // The intution is that->
-    // 1-  at every step it will only store water if its right and left steps are higher than the current step
-    // 2- they will store water units = min(leftMax,rightMax) - height of current step
-    
+// This is the optimal approach where we have optimised the calculation of leftMax and RightMax using 2-pointer approach
+// The intution here is we only need min(rightMax,LeftMax) so we move according to that
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> prefix(n,0);
-        vector<int> suffix(n,0);
-        int total = 0;
-        int maxH =0;
-        for(int i=0;i<height.size();i++){
-            maxH = max(maxH,height[i]);
-            prefix[i] = maxH;
-        }
 
-        int maxH2 = 0;
-        for(int i=n-1;i>=0;i--){
-            maxH2 = max(maxH2,height[i]);
-            suffix[i] = maxH2;
-        }
+        int l = 0;
+        int r = n-1;
+        int rightMax =0;
+        int leftMax =0;
+        int ans =0;
+        while(l<r){
+            rightMax = max(rightMax,height[r]);
+            leftMax = max(leftMax,height[l]);
 
-        for(int i=0;i<n;i++){
-            int leftMax = prefix[i];
-            int rightMax = suffix[i];
-
-            if(height[i]<leftMax && height[i]<rightMax){
-                total += min(leftMax,rightMax) - height[i];
+            if(rightMax<leftMax){
+                ans += rightMax-height[r];
+                r--;
+            }
+            else{
+                ans += leftMax-height[l];
+                l++;
             }
         }
 
-        return total;
-
+        return ans;
     }
 };
