5537064317a95a6f1aa1aad95dc0035ab1d45220
Aaryan Choudhary <aaryanchoudhary4647@gmail.com>
2026-08-18 19:39:31 +0530

    Time: 3 ms (27.61%), Space: 22.2 MB (18.55%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
-// This is the optimal soln as if we encounter a -ve value in stack there will be 4 cases in reverse traversal
-// 1- its next element will bigger and +ve
-// 2- its next element will be smaller and +ve
-// 3- its next element will be equal and +ve
-// 4- its next element will be -ve
+// same optimal approach but with forward traversal
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        
+        stack<int> s;
         int n = asteroids.size();
-        stack<int>s;
-        for(int i=n-1;i>=0;i--){
+
+        for(int i=0;i<n;i++){
+            if(asteroids[i]>0){s.push(asteroids[i]);}
+            else{
+
+                while(!s.empty() && s.top()>0 && s.top()<abs(asteroids[i])){
+                    s.pop();
+                }
+
+                if(!s.empty() && s.top() == abs(asteroids[i])){
+                    s.pop();
+                }
+                else if(s.empty() || s.top()<0){s.push(asteroids[i]);}
                 
-            if(asteroids[i]>0){
-                    while(!s.empty() && s.top()<0 && asteroids[i]>abs(s.top())){
-                        s.pop();
-                    }
-
-                    if(!s.empty() && s.top()<0 ){
-                        if(abs(s.top())==asteroids[i]){s.pop();}
-                        else{continue;}
-                    }
-                    else{s.push(asteroids[i]);}
             }
-            else{s.push(asteroids[i]);}
         }
-
         vector<int> ans;
-
         while(!s.empty()){
             ans.push_back(s.top());
             s.pop();
         }
+        reverse(ans.begin(),ans.end());
+        
 
         return ans;
     }
