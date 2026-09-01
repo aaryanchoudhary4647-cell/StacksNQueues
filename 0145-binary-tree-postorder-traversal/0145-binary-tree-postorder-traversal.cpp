fe2117202fa19259bab2fa326d1e6120a0d11101
Aaryan Choudhary <aaryanchoudhary4647@gmail.com>
2026-09-01 14:50:53 +0530

    Time: 0 ms (100%), Space: 11.1 MB (17.66%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,20 +11,34 @@
  */
 class Solution {
 public:
-    void traverse(TreeNode* root,vector<int>& ans){
-        if(root==NULL){return ;}
 
-        traverse(root->left,ans);
-        traverse(root->right,ans);
-        ans.push_back(root->val);
-    }
-
+// This is iterative approach using 2 stacks 
+// The intution is that we store in s2 as root-> right - > left an then we print it in l -> r -> root as s2 is a stack with LIFO
 
     vector<int> postorderTraversal(TreeNode* root) {
+        if(root==NULL){return {};}
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        s1.push(root);
         vector<int> ans;
 
-        traverse(root,ans);
+        while(!s1.empty()){
+            TreeNode* node = s1.top();
+            s1.pop();
+            s2.push(node);
+            if(node->left){s1.push(node->left);}
+            if(node->right){s1.push(node->right);}
+        }
+
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+
 
         return ans;
+
+
+
     }
 };
